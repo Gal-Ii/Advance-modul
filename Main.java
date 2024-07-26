935ef1da61ce6c5b3252b7e8aa9a24f4c2b6b880
Galina Georgieva <145717210+Gal-Ii@users.noreply.github.com>
2024-07-26 12:59:59 +0300

    f06_Defining_Classes-Opinion_Poll

--- Main.java
+++ Main.java
@@ -1,75 +1,41 @@
-package f06_Defining_Classes.Google;
+package f06_Defining_Classes.Opinion_Poll;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
+        List<Person> persons = new ArrayList<>();
 
-        List<Person> peopleList = new ArrayList<>();
+        int n = Integer.parseInt(scanner.nextLine());
 
-        while (!input.equals("End")) {
-            String[] inputArr = input.split("\\s+");
-            String personName = inputArr[0];
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            String name = input[0];
+            int age = Integer.parseInt(input[1]);
 
-            Person person = getPersonByName(peopleList, personName);
-
-            if (input.contains("company")) {
-                String companyName = inputArr[2];
-                String department = inputArr[3];
-                double salary = Double.parseDouble(inputArr[4]);
-
-                Company company = new Company(companyName, department, salary);
-                person.setCompany(company);
-
-            } else if (input.contains("pokemon")) {
-                String pokemonName = inputArr[2];
-                String type = inputArr[3];
-
-                Pokemon pokemon = new Pokemon(pokemonName, type);
-                person.addPokemon(pokemon);
-
-            } else if (input.contains("parents")) {
-                String parentName = inputArr[2];
-                String birthday = inputArr[3];
-
-                Relative parent = new Relative(parentName, birthday);
-                person.addParents(parent);
-
-            } else if (input.contains("children")) {
-                String childName = inputArr[2];
-                String birthday = inputArr[3];
-
-                Relative children = new Relative(childName, birthday);
-                person.addChildren(children);
-
-            } else if (input.contains("car")) {
-                String model = inputArr[2];
-                int speed = Integer.parseInt(inputArr[3]);
-                Car car = new Car(model, speed);
-                person.setCar(car);
-            }
-            input = scanner.nextLine();
+            Person person = getPersonByName(persons, name, age);
         }
-        String inputName = scanner.nextLine();
-        Person person = getPersonByName(peopleList, inputName);
-        System.out.println(person);
+        persons = persons.stream().filter(person -> person.getAge() > 30).collect(Collectors.toList());
+        persons.sort(Comparator.comparing(person -> person.getName()));
+
+        for (Person person : persons) {
+            System.out.println(person.toString());
+        }
+
     }
 
-    private static Person getPersonByName(List<Person> peopleList, String personName) {
-        for (Person person : peopleList) {
-            if (person.getName().equals(personName)) {
-                return person;
+    private static Person getPersonByName(List<Person> persons, String name, int age) {
+        for (Person p : persons) {
+            if(p.getName().equals(name)){
+                return p;
             }
         }
-        Person person = new Person(personName);
-        peopleList.add(person);
+        Person person = new Person(name, age);
+        persons.add(person);
         return person;
     }
 }
--- Person.java
+++ Person.java
@@ -1,86 +1,27 @@
-package f06_Defining_Classes.Google;
+package f06_Defining_Classes.Opinion_Poll;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
     private String name;
-    private Company company;
-    private List<Pokemon> pokemons;
-    private List<Relative> parents;
-    private List<Relative> childrens;
-    private Car car;
+    private int age;
 
-    public Person(String name){
+    public Person(String name, int age){
         this.name = name;
-        this.pokemons = new ArrayList<>();
-        this.parents = new ArrayList<>();
-        this.childrens = new ArrayList<>();
-
+        this.age = age;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setCar(Car car) {
-        this.car = car;
+    public int getAge() {
+        return age;
     }
 
-    public void setCompany(Company company) {
-        this.company = company;
+
+    public String toString(){
+        return String.format("%s - %d", this.name, this.age);
     }
-
-    public void addPokemon(Pokemon pokemon){
-        this.pokemons.add(pokemon);
-    }
-
-    public void addParents(Relative parents){
-        this.parents.add(parents);
-    }
-
-    public void addChildren(Relative children){
-        this.childrens.add(children);
-    }
-
-    public String toString() {
-
-        //GeorgeJohnson
-        //Company:
-        //JeleInc Jelior 777.77
-        //Car:
-        //AudiA4 180
-        //Pokemon:
-        //Onyx Rock
-        //Charizard Fire
-        //Parents:
-        //SaraJohnson 13/03/1933
-        //Children:
-        //SamJohnson 01/01/2001
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.name).append(System.lineSeparator());
-        sb.append("Company:").append(System.lineSeparator());
-        if (this.company != null) {
-            sb.append(this.company).append(System.lineSeparator());
-        }
-        sb.append("Car:").append(System.lineSeparator());
-        if (this.car != null) {
-            sb.append(this.car).append(System.lineSeparator());
-        }
-        sb.append("Pokemon:").append(System.lineSeparator());
-        if (!this.pokemons.isEmpty()) {
-            this.pokemons.forEach(pokemon -> sb.append(pokemon).append(System.lineSeparator()));
-        }
-        sb.append("Parents:").append(System.lineSeparator());
-        if (!this.parents.isEmpty()) {
-            this.parents.forEach(parent -> sb.append(parent).append(System.lineSeparator()));
-        }
-        sb.append("Children:").append(System.lineSeparator());
-        if (!this.childrens.isEmpty()) {
-            this.childrens.forEach(child -> sb.append(child).append(System.lineSeparator()));
-        }
-        return sb.toString();
-    }
-
 }
